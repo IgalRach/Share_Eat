49e67df38b68ce487933b2ed12f217dacda5bfb4
Igal Rachman <Igal Rachman@DESKTOP-GP5CVBJ>
2021-02-12 00:13:29 +0200

    added: Navigation-fragment All Post page with basic design Add post- title, ingredients,recipe,add photo button and official add for all the recipe.

---
+++ app/src/main/java/com/example/shareeat/AllPosts.java
@@ -0,0 +1,38 @@
+package com.example.shareeat;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.NavigableMap;
+
+
+public class AllPosts extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_all_posts, container, false);
+
+
+        // added button from all post to add post.
+        Button addBtn= view.findViewById(R.id.AllPost_addBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_allPosts_to_addPost );
+            }
+        });
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/shareeat/MainActivity.java
+++ app/src/main/java/com/example/shareeat/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.shareeat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity/* implements AdapterView.OnItemSelectedListener*/ {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+//        Spinner addPost_spinner= findViewById(R.id.addPost_Category);
+//        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this,R.array.Categories, android.R.layout.simple_spinner_item);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        addPost_spinner.setAdapter(adapter);
+//        addPost_spinner.setOnItemSelectedListener(this);
     }
+
+//    @Override
+//    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//        String addPost_text= parent.getItemAtPosition(position).toString();
+//        Toast.makeText(parent.getContext(),addPost_text,Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    public void onNothingSelected(AdapterView<?> parent) {
+//
+//    }
 }
