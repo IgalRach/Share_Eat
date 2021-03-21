638ddfbb0886642fd8eed963957586a210e5974f
Igal Rachman <Igal Rachman@DESKTOP-GP5CVBJ>
2021-03-21 13:05:26 +0200

    updated all the changes from the merges

--- app/src/main/java/com/example/shareeat/login.java
+++ app/src/main/java/com/example/shareeat/login.java
@@ -4,11 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import com.example.shareeat.model.ModelFirebase;
 import com.google.firebase.auth.FirebaseAuth;
@@ -66,11 +65,11 @@ public class login extends AppCompatActivity {
         moveToRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    startActivity(new Intent(login.this,register.class));
+                startActivity(new Intent(login.this,register.class));
             }
         });
 
-}
+    }
 
     private void toRegisterPage() {
         Intent intent = new Intent(this, register.class);
--- app/src/main/java/com/example/shareeat/register.java
+++ app/src/main/java/com/example/shareeat/register.java
@@ -4,84 +4,57 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 
 import com.example.shareeat.model.ModelFirebase;
-import com.example.shareeat.model.User;
-
 
 public class register extends AppCompatActivity {
 
-    EditText nickname, email, password;
-    Button btnLogin, btnRegister;
-    ProgressBar progressBar;
+    EditText fullNameInput;
+    EditText passwordInput;
+    EditText emailInput;
+    Button registerBtn;
+    Button moveToLoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        nickname = findViewById(R.id.register_nickname);
-        password = findViewById(R.id.register_password);
-        email = findViewById(R.id.register_email);
+        this.setTitle("Sign Up");
 
-        btnLogin = findViewById(R.id.register_login_btn);
-        btnRegister = findViewById(R.id.register_register_btn);
+        fullNameInput = findViewById(R.id.register_fullName);
+        passwordInput = findViewById(R.id.register_password);
+        emailInput = findViewById(R.id.register_email);
+        registerBtn = findViewById(R.id.register_register_btn);
 
-        progressBar = findViewById(R.id.registerfrag_progressBar);
-
-        btnRegister.setOnClickListener(new View.OnClickListener(){
-
+        registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String userNickname = nickname.getText().toString().trim();
-                String userEmail = email.getText().toString().trim();
-                String userPassword = password.getText().toString().trim();
+                ModelFirebase.registerUserAccount(fullNameInput.getText().toString(), passwordInput.getText().toString(),
+                        emailInput.getText().toString(), new ModelFirebase.Listener<Boolean>() {
+                            @Override
+                            public void onComplete() {
+                                startActivity(new Intent(register.this, MainActivity.class));
+                                finish();
+                            }
 
-
-                if(userNickname.isEmpty()){
-                    nickname.setError("Nickname is required!");
-                    nickname.requestFocus();
-                    return;
-                }
-
-                if(userEmail.isEmpty()){
-                    email.setError("Email is required!");
-                    email.requestFocus();
-                    return;
-                }
-
-                if(!Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()){
-                    email.setError("Please provide valid email!");
-                    email.requestFocus();
-                    return;
-                }
-
-                if(userPassword.isEmpty()){
-                    password.setError("Password is required!");
-                    password.requestFocus();
-                    return;
-                }
-                if(userPassword.length()<8 || userPassword.length()>20){
-                    password.setError("Password length must be 8-20 characters!");
-                    password.requestFocus();
-                    return;
-                }
-
-                progressBar.setVisibility(View.VISIBLE);
-                final User user = new User(userNickname, userEmail);
-                ModelFirebase.signUpToFirebase(user,userPassword,register.this);
-                startActivity(new Intent(register.this,MainActivity.class));
-
+                            @Override
+                            public void onFail() {
+                            }
+                        });
             }
         });
 
+        moveToLoginBtn = findViewById(R.id.register_login_btn);
+        moveToLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(register.this, login.class);
+                startActivity(intent);
+            }
+        });
     }
-
-
-
 }
