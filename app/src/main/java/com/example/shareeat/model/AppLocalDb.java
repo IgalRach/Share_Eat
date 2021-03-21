dc24a909dcd481b21e895a8063a4ad00b3ab537b
Igal Rachman <Igal Rachman@DESKTOP-GP5CVBJ>
2021-03-21 12:20:46 +0200

    designed the login and register pages

---
+++ app/src/main/java/com/example/shareeat/model/AppLocalDb.java
@@ -0,0 +1,23 @@
+package com.example.shareeat.model;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.shareeat.MyApplication;
+
+
+@Database(entities = {Recipe.class}, version = 1)
+abstract class AppLocalDbRepository extends RoomDatabase {
+    public abstract RecipeDao recipeDao();
+}
+
+public class AppLocalDb{
+
+    static public AppLocalDbRepository db =
+            Room.databaseBuilder(MyApplication.context,
+                    AppLocalDbRepository.class,
+                    "dbFileName.db")
+                    .fallbackToDestructiveMigration()
+                    .build();
+}
