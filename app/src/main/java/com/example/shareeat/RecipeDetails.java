7465bf2c36e53a15b9cb49436086c29a52a26ad0
Omri <yehidi@gmail.com>
2021-04-03 14:53:22 +0300

    Added dropdown style Fix Styles

--- app/src/main/java/com/example/shareeat/RecipeDetails.java
+++ app/src/main/java/com/example/shareeat/RecipeDetails.java
@@ -15,7 +15,9 @@ import android.widget.TextView;
 import com.example.shareeat.model.Model;
 import com.example.shareeat.model.Recipe;
 
-public class RecipeDetails extends AddPost {
+import java.io.File;
+
+public class RecipeDetails extends Fragment {
 
     String recipeId;
     Recipe rcp;
@@ -24,26 +26,26 @@ public class RecipeDetails extends AddPost {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = super.onCreateView(inflater, container, savedInstanceState);
-        title = view.findViewById(R.id.addPost_Title);
-        title.setText("Recipe Details");
+        View view= inflater.inflate(R.layout.fragment_recipe_details, container, false);
+//        title = view.findViewById(R.id.addPost_Title);
+//        title.setText("Recipe Details");
 
-        recipeNameEditText.setEnabled(false);
-        recipeEditText.setEnabled(false);
-        spinner.setEnabled(false);
-
-        recipeEditText.setBackgroundColor(Color.parseColor("#FFFFFF"));
-
-        addBtn.setVisibility(View.INVISIBLE);
-        Imageurl.setVisibility(View.INVISIBLE);
+//        recipeNameEditText.setEnabled(false);
+//        recipeEditText.setEnabled(false);
+//        spinner.setEnabled(false);
+//
+//        recipeNameEditText.setBackgroundColor(Color.parseColor("#D2000000"));
+//        recipeEditText.setBackgroundColor(Color.parseColor("#D2000000"));
+//        addBtn.setVisibility(View.INVISIBLE);
+//        Imageurl.setVisibility(View.INVISIBLE);
         recipeId = RecipeDetailsArgs.fromBundle(getArguments()).getRecipeId();
 
         Model.instance.getRecipe(recipeId, new Model.GetRecipeListener() {
             @Override
             public void onComplete(Recipe recipe) {
                 rcp = recipe;
-                recipeNameEditText.setText(rcp.getTitleRecipe());
-                recipeEditText.setText(rcp.getRecipe());
+//                recipeNameEditText.setText(rcp.getTitleRecipe());
+//                recipeEditText.setText(rcp.getRecipe());
             }
         });
 
